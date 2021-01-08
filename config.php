a192617265025ddd267943256c6288a8f723ed54
WillyFaq <willyfaqurokhim@gmail.com>
2021-01-08 14:56:40 +0700

    change

--- config.php
+++ config.php
@@ -11,7 +11,7 @@ function d_url($s) {
     return base64_decode(str_pad(strtr($s, '-_', '+/'), strlen($s) % 4, '=', STR_PAD_RIGHT));
 }
 
-#$api = "https://apigrabber.herokuapp.com/";
-$api = "http://127.0.0.1:5000/";
+$api = "https://apigrabber.herokuapp.com/";
+#$api = "http://127.0.0.1:5000/";
 $GLOBALS['api'] = $api;
 ?>
